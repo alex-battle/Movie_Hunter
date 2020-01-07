e41bedb87c953c8dec8a2b24a1ef1a5b1954b9ef
Alex-makk <58659536+Alex-makk@users.noreply.github.com>
2020-01-07 17:12:17 -0500

    api completely working and doing everything I want it to do."

--- script.js
+++ script.js
@@ -7,20 +7,40 @@ function getMov(event){
     event.preventDefault()
     let search= document.querySelector(".searchBar").value
     console.log(search)
+    
 
-    let url= (`http://www.omdbapi.com/?t=${search}&apikey=9ef749a4`)
+    let url = (`http://www.omdbapi.com/?t=${search}&apikey=9ef749a4`)
     console.log(url)
-
+    
 
     let movieData= axios.get(url).then(res =>{
         console.log(res.data)
 
-        let appendMovie= document.querySelector('.movie-info')
-        appendMovie.append(res.data["Title"]);
-        appendMovie.append(res.data["Year"]);
-        appendMovie.append(res.data["Genre"]);       
-        appendMovie.append(res.data["Runtime"]);
-        appendMovie.append(res.data["Plot"]);
-    })
+        let titleMovie= document.querySelector('#title')
+        titleMovie.innerHTML=""
+        titleMovie.append(res.data["Title"]);
+    
+        let yearMovie= document.querySelector('#year')
+        yearMovie.innerHTML=""
+        yearMovie.append(res.data["Year"]);
+        let genreMovie= document.querySelector('#genre')
+        genreMovie.innerHTML=""
+        genreMovie.append(res.data["Genre"]);      
+        let runtimeMovie= document.querySelector('#runtime')
+        runtimeMovie.innerHTML="" 
+        runtimeMovie.append(res.data["Runtime"]);
+        let plotMovie= document.querySelector('#plot')
+        plotMovie.innerHTML=""
+        plotMovie.append(res.data["Plot"]);
+        let poster= res.data["imdbID"]
+        let url2 = (`http://img.omdbapi.com/?i=${poster}&h=350&apiKey=9ef749a4`)
+        
+        let moviePoster= document.querySelector('.poster')
+        moviePoster.innerHTML=`
+        <img src=${url2} />
+        `
+        // moviePoster.append(url2);
 
+    })
 }
+
